a5c1299e5feca6b88b866a46df1278dc94f0a325
Tajanae Boone <Tajanaeboone@yahoo.com>
2021-03-24 20:21:16 -0400

    java commit

--- src/main/java/com/codedifferently/NumberUtilities.java
+++ src/main/java/com/codedifferently/NumberUtilities.java
@@ -2,30 +2,67 @@ package com.codedifferently;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String output = "";
+        for (int i = start; i <= stop; i++) {
+            if (i % 2 != 0) {
+                output += i;
+            }
+        }
+        return output;
     }
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String output = "";
+        for (int i = start; i <= stop; i++) {
+            if (i % 2 == 0) {
+                output += i;
+            }
+        }
+        return output;
     }
+
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
-    }
+        String output = "";
+        for (int i = start; i < stop; i+=step) {
 
-    public static String getRange(int start) {
-        return null;
+            output += i * i;
+        }
+        return output;
+    }
+    public static String getRange(int stop) {
+        String output = "";
+        for( int i = 0; i < stop; i++){
+            output += i;
+        }
+        return output;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String output = "";
+        for( int i = start; i < stop; i++){
+            output += i;
+        }
+        return output;
     }
+
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String output = "";
+        for (int i = start; i < stop; i+=step) {
+            output += i;
+        }
+        return output;
     }
 
+
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String output = "";
+        for (int i = start; i < stop; i+=step) {
+
+            output += (int) Math.pow(i, exponent);
+        }
+        return output;
     }
+
 }
